bcf3fe89a8669e3545323482a376da711dcbbd96
Chidanand26 <01fe24bar027@kletech.ac.in>
2025-05-10 10:03:10 +0530

    Initial commit with starter files

---
+++ logiconnect-starter/logiconnect/frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to LogiConnect</h1>
+      <p>Your MSME logistics coordination platform.</p>
+    </div>
+  );
+}
+
+export default App;
